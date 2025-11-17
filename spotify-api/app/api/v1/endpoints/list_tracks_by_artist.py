dc3cfe0fe22bea5d7aa43258f537a592bd6c6afd
G-coder <gregoire.woroniak@gmail.com>
2025-11-17 21:08:43 +0100

    [FIX ]add api schemas to remove 'Unprocessable Entity' error

--- spotify-api/app/api/v1/endpoints/list_tracks_by_artist.py
+++ spotify-api/app/api/v1/endpoints/list_tracks_by_artist.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter, Depends
 from app.core.spotify_client import SpotifyClient, get_spotify_client
+from app.schema.spotify_api_schemas import ListTracksRequest
 
 router = APIRouter()
 
 @router.post("/", summary="")
 async def list_tracks_by_artist(
-    artist_name: str,
+    request: ListTracksRequest,
     spotifyClient: SpotifyClient = Depends(get_spotify_client)
 ):
+    artist_name = request.artist_name
     data = spotifyClient.get(endpoint=f"v1/search?q={artist_name}&type=artist&limit=1")
 
     if (not data["artists"]["items"] or len(data["artists"]["items"]) == 0):
