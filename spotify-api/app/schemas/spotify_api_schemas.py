25c818fef9cbb7cbb3fbfaf145d96aa019544054
G-coder <gregoire.woroniak@gmail.com>
2025-11-21 22:06:01 +0100

    remove useless fastAPI

--- spotify-api/app/schemas/spotify_api_schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class ListTracksRequest(BaseModel):
-    artist_name: str
-
-class CreatePlaylistRequest(BaseModel):
-    name: str
-    desc: str
-    public: bool = False
